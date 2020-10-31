56ea35c8679d8cb2d291523b546aee23b38aa58c
4thel00z <4thel00z@gmail.com>
2020-10-31 03:14:45 +0100

    [master] Fix create_post: remove trailing '-'

--- create_post.sh
+++ create_post.sh
@@ -2,11 +2,11 @@
 
 
 cat > _posts/$(date --rfc-3339=date)-$2 <<EOF
-----
--layout: post
--title:  "$1"
--date:  $(date --rfc-3339=seconds)
--categories: 
--comments: true
-----
+---
+layout: post
+title:  "$1"
+date:  $(date --rfc-3339=seconds)
+categories: 
+comments: true
+---
 EOF
